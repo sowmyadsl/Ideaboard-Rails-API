95610abb6fa0bfa663cbbabfc0ec2e461ee9bdec
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-09-04 04:42:24 -0700

    Create a idea model and seed the file and create an api and tested it

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+ideas = Idea.create(
+[
+  {
+  title: "A new recipe",
+  body: "Made of chocalate"
+  },
+  {
+    title: "A twitter clone",
+    body: "Using react on rails"
+  },
+  {
+    title: "A comic novel",
+    body: "Like friends"
+  },
+  {
+    title:"Card game design",
+    body: "like UNO "
+  }
+])
