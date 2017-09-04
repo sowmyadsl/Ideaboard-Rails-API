8a71de2e38f96c378dd52a2266e28ad7877261ae
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-09-04 11:22:08 -0700

    Add a create method and let the ideas display in descending orde on the front end

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module IdeaboardApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -29,5 +30,14 @@ module IdeaboardApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rails 5
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
